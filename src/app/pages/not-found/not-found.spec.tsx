c397bc0d664eb49f569f4d18b8b5a2c8f86267ad
Yassel Avila <yassel.avila@gmail.com>
2020-09-17 17:16:10 -0400

    Add the 404-error page

---
+++ src/app/pages/not-found/not-found.spec.tsx
@@ -0,0 +1,16 @@
+// import { render } from '@testing-library/react';
+// import React from 'react';
+import NotFound from './not-found';
+
+// TODO
+describe('Not Found', () => {
+  it('should be defined', () => {
+    expect(NotFound).toBeTruthy();
+  });
+
+  it.todo('should render successfully');
+  // it('should render successfully', () => {
+  //   const { baseElement } = render(<NotFound>Test</NotFound>);
+  //   expect(baseElement).toBeTruthy();
+  // });
+});
---
+++ src/app/pages/not-found/not-found.tsx
@@ -0,0 +1,51 @@
+import { Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import React, { ReactElement, useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
+import { ContentWrapper } from '../../core/components/content-wrapper/content-wrapper';
+
+const useStyles = makeStyles(({ palette, spacing }: Theme) => ({
+  loginButton: {
+    textTransform: 'none',
+    fontSize: 16,
+    fontWeight: 'normal',
+    marginTop: spacing(4)
+  },
+  the404: {
+    color: palette.text.secondary,
+    fontWeight: 'bold'
+  }
+}));
+
+export default function NotFound(): ReactElement {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const handleClick = useCallback(() => history.push('/share-feedback'), [history]);
+
+  return (
+    <ContentWrapper>
+      <Typography variant="body1" className={classes.the404}>
+        404
+      </Typography>
+
+      <Typography variant="h4">
+        Sorry! The page you are looking for cannot be found.
+        <span role="img" aria-labelledby="Sad">
+          😢
+        </span>
+      </Typography>
+
+      <Button
+        variant="contained"
+        size="large"
+        color="primary"
+        className={classes.loginButton}
+        onClick={handleClick}
+      >
+        Back to Share Feedback
+      </Button>
+    </ContentWrapper>
+  );
+}
